432bf3638e1c6fb4a0f91760ebbe446972cb5671
rahul <arahulv@iitk.ac.in>
2021-07-06 21:15:33 +0530

    history table added , tax added , token validation for transfer , awardcoin added ,allow tranfer if their is  participation by studnt

--- cmd/iitk-coin/main.go
+++ cmd/iitk-coin/main.go
@@ -11,13 +11,22 @@ import (
 )
 
 func main() {
-
+	// db, err := sql.Open("sqlite3", "history.db")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer db.Close()
+	// query, err := db.Prepare("CREATE TABLE IF NOT EXISTS history (fromroll INTEGER  NOT NULL,toroll INTEGER NOT NULL,coin INTEGER UNSIGNED check(coin >=0) ,date TEXT,remark TEXT )")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// query.Exec()
 	http.HandleFunc("/login", handle.Login)
 	http.HandleFunc("/signup", handle.Signup)
 	http.HandleFunc("/secretpage", handle.SecretPage)
-	http.HandleFunc("/balance", handle.CheckBalance) // Get request
+	http.HandleFunc("/balance", handle.SecretPage) // Get request
 	http.HandleFunc("/transfer", handle.TransferCoin)
-	http.HandleFunc("/award", handle.AwardCoin)
+	http.HandleFunc("/award", handle.SecretPage)
 	fmt.Println("server running")
 	http.ListenAndServe(":8080", nil)
 
--- http/handle.go
+++ http/handle.go
@@ -22,6 +22,8 @@ type Person struct {
 	Name     string
 	Rollno   int
 	Password string
+	Batch    string
+	Admin    bool
 }
 type LoginCred struct {
 	Rollno   int
@@ -62,26 +64,43 @@ func UserExists(rollno int) bool {
 	return false
 
 }
+func AddHistory(Fromrollno int, Torollno int, coin int, Datetime string, remark string) {
+	db, err := sql.Open("sqlite3", "history.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	query, err := db.Prepare("CREATE TABLE IF NOT EXISTS hist (fromroll INTEGER  NOT NULL,toroll INTEGER NOT NULL,coin INTEGER UNSIGNED check(coin >=0) ,date TEXT,remark TEXT )")
+	if err != nil {
+		panic(err)
+	}
+	query.Exec()
+	query, err = db.Prepare("INSERT INTO hist (fromroll,toroll,coin ,date,remark) VALUES ( ?, ?,?,?,?)")
+	if err != nil {
+		panic(err)
+	}
+	// query.Exec()
+	query.Exec(Fromrollno, Torollno, coin, Datetime, remark)
+	// fmt.Println(Fromrollno, Torollno, coin, Datetime, remark)
 
-// func LoginUser(data LoginCred) {
-
-// }
+}
 func SignUpUser(data Person) {
 	db, err := sql.Open("sqlite3", "data.db")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	query, err := db.Prepare("CREATE TABLE IF NOT EXISTS user (rollno INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL,password TEXT NOT NULL,coin INTEGER UNSIGNED check(coin >=0 ))")
+	query, err := db.Prepare("CREATE TABLE IF NOT EXISTS user (rollno INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL,password TEXT NOT NULL,coin INTEGER UNSIGNED check(coin >=0 ) ,  batch TEXT NOT NULL , admin BOOLEAN)")
 	if err != nil {
 		panic(err)
 	}
 	query.Exec()
-	query, err = db.Prepare("INSERT INTO user (rollno, name, password,coin) VALUES (?, ?, ?,?)")
+	query, err = db.Prepare("INSERT INTO user (rollno, name, password,coin,batch,admin) VALUES (?, ?, ?,?,?,?)")
 	if err != nil {
 		panic(err)
 	}
-	query.Exec(data.Rollno, data.Name, data.Password, 10)
+	query.Exec(data.Rollno, data.Name, data.Password, 10, data.Batch, data.Admin)
+	// fmt.Println(data.Admin)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -162,6 +181,7 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 	hashp := hashAndSalt([]byte(p.Password))
 	// log.Println(hashp)
 	p.Password = hashp
+	// fmt.Println(p.Admin, " - ")
 	SignUpUser(p)
 }
 
@@ -193,7 +213,13 @@ func SecretPage(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
-	fmt.Fprintf(w, "%d logged in", claims.Rollno)
+	switch r.URL.Path {
+	case "/balance":
+		CheckBalance(w, r)
+	case "/award":
+		AwardCoin(w, r)
+	}
+
 }
 
 func TransferCoin(w http.ResponseWriter, r *http.Request) {
@@ -218,11 +244,87 @@ func TransferCoin(w http.ResponseWriter, r *http.Request) {
 		mutex.Unlock()
 		panic("reciever does not exist")
 	}
+	db1, err := sql.Open("sqlite3", "history.db")
+	if err != nil {
+		mutex.Unlock()
+		panic(err)
+	}
+	var rew_hist struct {
+		fromroll_rew int
+		toroll_rew   int
+	}
+	// to check if students has participated in events by counting no of rewards they get
+	query := db1.QueryRow("SELECT COUNT(*) FROM hist WHERE toroll IN($1) AND remark IN ('reward')  ", p.Fromrollno)
+
+	err = query.Scan(&rew_hist.fromroll_rew)
+	if err != nil {
+		// If an entry with the username does not exist, send an "Unauthorized"(401) status
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// If the error is of any other type, send a 500 status
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	query = db1.QueryRow("SELECT  COUNT(*) FROM hist WHERE toroll IN($1) AND remark IN ('reward')  ", p.Torollno)
+
+	err = query.Scan(&rew_hist.toroll_rew)
+	if err != nil {
+		// If an entry with the username does not exist, send an "Unauthorized"(401) status
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// If the error is of any other type, send a 500 status
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// fmt.Println(rew_hist.fromroll_rew, "-----", rew_hist.toroll_rew)
+	if rew_hist.fromroll_rew == 0 || rew_hist.toroll_rew == 0 {
+		panic("participate in events to transfer money")
+	}
 	ctx := context.Background()
 	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
 		mutex.Unlock()
 		log.Fatal(err)
+		return
+	}
+
+	query = db.QueryRow("SELECT batch FROM user WHERE rollno=$1", p.Fromrollno)
+	if err != nil {
+		panic(err)
+	}
+	var storedCoins struct {
+		FromBatch string
+		ToBatch   string
+	}
+
+	err = query.Scan(&storedCoins.FromBatch)
+
+	if err != nil {
+		// If an entry with the username does not exist, send an "Unauthorized"(401) status
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// If the error is of any other type, send a 500 status
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	query = db.QueryRow("SELECT batch FROM user WHERE rollno=$1", p.Torollno)
+	err = query.Scan(&storedCoins.ToBatch)
+
+	if err != nil {
+		// If an entry with the username does not exist, send an "Unauthorized"(401) status
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// If the error is of any other type, send a 500 status
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -239,6 +341,11 @@ func TransferCoin(w http.ResponseWriter, r *http.Request) {
 		tx.Rollback()
 		return
 	}
+	if storedCoins.FromBatch == storedCoins.ToBatch {
+		p.Coin = (p.Coin * 98) / 100
+	} else {
+		p.Coin = (p.Coin * 67) / 100
+	}
 
 	_, err = tx.ExecContext(ctx, "UPDATE user SET coin=coin+$1 WHERE rollno=$2", p.Coin, p.Torollno)
 	if err != nil {
@@ -248,6 +355,7 @@ func TransferCoin(w http.ResponseWriter, r *http.Request) {
 	}
 	err = tx.Commit()
 	mutex.Unlock()
+	AddHistory(p.Fromrollno, p.Torollno, p.Coin, time.Now().Format("2006.01.02 15:04:05"), "transfer")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -292,6 +400,7 @@ func CheckBalance(w http.ResponseWriter, r *http.Request) {
 
 }
 func AwardCoin(w http.ResponseWriter, r *http.Request) {
+
 	var p struct {
 		Rollno int
 		Award  int
@@ -307,11 +416,22 @@ func AwardCoin(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	// defer db.Close()
+	defer db.Close()
 	ctx := context.Background()
 	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
 		log.Fatal(err)
+		return
+	}
+
+	if err != nil {
+		// If an entry with the username does not exist, send an "Unauthorized"(401) status
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// If the error is of any other type, send a 500 status
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -324,4 +444,6 @@ func AwardCoin(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	AddHistory(0, p.Rollno, p.Award, time.Now().Format("2006.01.02 15:04:05"), "reward")
+
 }
